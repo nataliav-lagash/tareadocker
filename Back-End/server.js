c4d38faa27e04a596abe198ef7e6ba13e0fe0c30
nataliav-lagash <nataliav@lagash.com>
2020-01-09 19:45:15 -0300

    ashadh

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 let { HOST, PORT, AMBIENTE } = process.env;
+const mariadb = require("mariadb");
 
 console.log(process.env);
 
@@ -36,6 +37,44 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
+  res.send(
+    `API construida para Lagash University se encuentra arriba en ambiente ${AMBIENTE ||
+      "No definido"}`
+  );
+});
+
+app.get("/mariaGet", (req, res) => {
+  const pool = mariadb.createPool({
+    host: "localhost",
+    user: "root",
+    port: 8080,
+    password: "admin",
+    connectionLimit: 5
+  });
+  pool
+    .getConnection()
+    .then(conn => {
+      conn
+        .query("SELECT * from Agencia")
+        .then(rows => {
+          console.log(rows); //[ {val: 1}, meta: ... ]
+          //Table must have been created before
+          // " CREATE TABLE myTable (id int, val varchar(255)) "
+          //return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+        })
+        .then(res => {
+          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err);
+          conn.end();
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
   res.send(
     `API construida para Lagash University se encuentra arriba en ambiente ${AMBIENTE ||
       "No definido"}`
